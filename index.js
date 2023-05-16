614b043e491de2deac4d64d8c75fa66d55555f16
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-05-16 15:37:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstName= "Cheslyn";
+let lastName= "Herman";
+
+let a= firstName+lastName;
+document.write(a);
+
+let numberVar= 5;
+console.log(numberVar * 5);
+
+const name= 'Cheslyn';
+let name=surname;
+console.log(surname);
